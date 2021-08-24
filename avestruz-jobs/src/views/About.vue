a2bdf8f56fef21bc75db34e2a8b974e552ded270
jansevero <46618122+jansevero@users.noreply.github.com>
2021-08-24 17:34:47 -0300

    a

---
+++ avestruz-jobs/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, itaque iste cum et quis saepe labore neque excepturi quidem enim harum maxime dicta ad! Qui laborum nobis consectetur at sequi?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro obcaecati, aperiam aliquid vel iste libero perspiciatis provident quo. Suscipit id quia, distinctio inventore mollitia iste nihil eum necessitatibus nesciunt neque.</p>
+  </div>
+</template>
---
+++ avestruz-jobs/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, itaque adipisci. Eaque explicabo a ipsam maxime rem, dolorum facilis voluptatibus commodi aut non suscipit at architecto sed sunt, harum dolores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum at tenetur voluptate corporis iste porro hic necessitatibus maxime saepe natus magnam nulla adipisci, architecto, ab cupiditate quia, nihil veritatis explicabo.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
